d0494fddf802a6e1c073e7ab5e797fc96f0b73b3
Suman11Gupta <141143670+Suman11Gupta@users.noreply.github.com>
2025-04-07 02:04:37 +0530

    Update UserServiceImpl.java

--- Quiz_Project/src/main/java/com/example/Quiz_Project/service/user/UserServiceImpl.java
+++ Quiz_Project/src/main/java/com/example/Quiz_Project/service/user/UserServiceImpl.java
@@ -1,8 +1,34 @@
 package com.example.Quiz_Project.service.user;
 
+import com.example.Quiz_Project.entities.User;
+import com.example.Quiz_Project.enums.UserRole;
+import com.example.Quiz_Project.repository.UserRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService{
+  
+  @Autowired
+    private UserRepository userRepository;
+
+    @PostConstruct
+    private void createAdminUser() {
+        User optionalUser = userRepository.findByRole(UserRole.ADMIN);
+        if (optionalUser == null){
+            User user= new User();
+            user.setName("Admin");
+            user.setEmail("admin@gmail.com");
+            user.setRole(UserRole.ADMIN);
+            user.setPassword("admin");
+
+            userRepository.save(user);
+        }
+    }
+
+    public Boolean hasUserWithEmail(String email) {
+        return userRepository.findFirstByEmail(email) != null;
+    }
 
 }
