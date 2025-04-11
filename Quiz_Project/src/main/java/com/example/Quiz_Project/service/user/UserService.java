34badd143a3d92277350f886c3da6e70c1862c57
Suman11Gupta <141143670+Suman11Gupta@users.noreply.github.com>
2025-04-11 17:09:30 +0530

    Login API

--- Quiz_Project/src/main/java/com/example/Quiz_Project/service/user/UserService.java
+++ Quiz_Project/src/main/java/com/example/Quiz_Project/service/user/UserService.java
@@ -2,7 +2,9 @@ package com.example.Quiz_Project.service.user;
 
 public interface UserService {
   
-   User createUser(User user);
+   User createUser(User user); //Create user
 
-    Boolean hasUserWithEmail(String email);
+   Boolean hasUserWithEmail(String email); //Admin
+
+    User login(User user); // Login Api
 }
