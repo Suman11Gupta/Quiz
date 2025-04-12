44936d02d1bf9e10fe56ac192e82bbf59636067c
Suman11Gupta <suman1120gupta@gmail.com>
2025-04-12 15:08:02 +0530

    change

---
+++ src/app/modules/auth/services/auth.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const Basic_URL = "http://localhost:8080/";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http:HttpClient) { }
+
+  register(data): Observable<any>{
+    return this.http.post(Basic_URL + "api/auth/sign-up", data);
+  }
+
+  login(loginRequest: any): Observable<any>{
+    return this.http.post(Basic_URL + "api/auth/login", loginRequest);
+  }
+}
---
+++ src/app/modules/shared/shared.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { DemoNgZorroAntdModule } from '../../DemoNgZorroAndModule';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterOutlet,
+    RouterLink,
+    DemoNgZorroAntdModule
+  ], exports:[
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterOutlet,
+    RouterLink,
+    DemoNgZorroAntdModule
+  ]
+})
+export class SharedModule { }
