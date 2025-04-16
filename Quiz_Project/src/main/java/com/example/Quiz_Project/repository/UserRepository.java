6e64480e5d7698d3deccd6c34213bf90c0bf7b14
Suman11Gupta <141143670+Suman11Gupta@users.noreply.github.com>
2025-04-16 12:58:53 +0530

    Update UserRepository.java

--- Quiz_Project/src/main/java/com/example/Quiz_Project/repository/UserRepository.java
+++ Quiz_Project/src/main/java/com/example/Quiz_Project/repository/UserRepository.java
@@ -1,15 +1,15 @@
 package com.example.Quiz_Project.repository;
 import com.example.Quiz_Project.entities.User;
+import com.example.Quiz_Project.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
 User findByRole(UserRole role);
-  
 User findFirstByEmail(String email);
 
 Optional<User> findByEmail(String email);
-  
 }
